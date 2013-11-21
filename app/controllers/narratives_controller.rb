7794ba444a2f14bd963a155fb916dc649b94b641
Jennie Louie <jennie.louie@gmail.com>
2013-11-20 19:02:40 -0800

    added narratives and writers controllers, added controller actions

--- app/controllers/narratives_controller.rb
+++ app/controllers/narratives_controller.rb
@@ -1,2 +1,35 @@
 class NarrativesController < ApplicationController
+
+  def index
+    @narratives = Narrative.all
+  end
+
+  def new
+    @narrative = Narrative.new
+  end
+
+  def create
+    new_narrative = Narrative.create(params[:narrative])
+    redirect to narratives_path(new_narrative)
+  end
+
+  def show
+    @narrative = Narrative.find(params[:id])
+  end
+
+  def edit
+    @narrative = Narrative.find(parms[:id])
+  end
+
+  def update
+    revised_narrative = Narrative.find(params[:id])
+    revised_narrative.update_attributes(params[:narrative])
+    redirect_to narrative_path(revised_narrative)
+  end
+
+  def destroy
+    Narrative.delete(params[:id])
+    redirect_to narratives_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Mango::Application.routes.draw do
   root to: "feedbacks#index"
 
   resources :feedbacks
+  resources :narratives
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
